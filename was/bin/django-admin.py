5ff1721d422e7e6604989046a39442b33b1b75da
Jack Couzin <3118951C@student.gla.ac.uk>
2026-03-11 14:56:06 +0000

    Removed my venv and added to gitignore

--- was/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jack/Documents/UNI/WAS GROUP TASK/reciperate_project/was/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
