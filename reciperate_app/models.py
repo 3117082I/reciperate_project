90d0d866156357eb9847fff3623b786cc05c736e
Jack Couzin <3118951C@student.gla.ac.uk>
2026-03-12 11:39:29 +0000

    Created user and likes database table, also updated likes functionality in recipe table to allow viewing users liked recipes.

--- reciperate_app/models.py
+++ reciperate_app/models.py
@@ -15,11 +15,27 @@ class Recipe(models.Model):
     image = models.ImageField(upload_to='recipe_images/', blank=True, null=True)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
-    likes = models.IntegerField(default=0)
 
     @property
     def like_count(self):
-        return self.liked_by.count()
+        return self.likes.count()
 
     def __str__(self):
         return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.name
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='liked_recipes')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='likes')
+    liked_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'recipe')
+
+    def __str__(self):
+        return f'{self.user.username} likes {self.recipe.name}'
