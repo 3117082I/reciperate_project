192fa93afb112658a64db34225275e45bef0fa08
Hugh Semple <3136816S@student.gla.ac.uk>
2026-03-13 14:52:03 +0000

    Added SignUpForm() and SignInForm() to forms.py

--- reciperate_app/forms.py
+++ reciperate_app/forms.py
@@ -1,5 +1,19 @@
 from django import forms
 from .models import Recipe
+from django.contrib.auth.models import User
+
+class SignUpForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        field = ('username', 'password')
+
+class SignInForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput()
+
+
 
 class RecipeForm(forms.ModelForm):
     class Meta:
