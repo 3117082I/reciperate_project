691c2c269cbcf79fed399771c2860fe4db148101
Jack Couzin <3118951C@student.gla.ac.uk>
2026-03-17 14:45:39 +0000

    Added image to home page and working on population script.

--- populate.py
+++ populate.py
@@ -39,7 +39,10 @@ def create_like(user, recipe):
 def populate():
     #users
     jack = create_user('jack', 'Password123!')
-    phil = create_user('phil', 'mypassword')
+    hugh = create_user('hugh', 'Password456!')
+    nadia = create_user('nadia', 'Password789!')
+    hafsa = create_user('hafsa', 'Password1234!')
+    zach = create_user('zach', 'Password1234!')
 
     #recipes
     pancakes = create_recipe(
@@ -49,17 +52,97 @@ def populate():
         category='Breakfast'
     )
 
+    french_toast =create_recipe(
+        user=zach,
+        name='French Toast',
+        url='https://www.bbcgoodfood.com/recipes/french-toast',
+        category='Breakfast'
+    )
+
+    pain_au_chocolat = create_recipe(
+        user=nadia,
+        name='Pain Au Chocolat',
+        url='https://www.bbcgoodfood.com/recipes/pain-au-chocolat',
+        category='Breakfast'
+    )
+
+    cheese_omelet = create_recipe(
+        user=hugh,
+        name='Cheese Omelet',
+        url='https://www.bbcgoodfood.com/recipes/cheese-omelette',
+        category='Breakfast'
+    )
+
+    falafel_burgers = create_recipe(
+        user=hafsa,
+        name='Falafel Burgers',
+        url='https://www.bbcgoodfood.com/recipes/falafel-burgers-0',
+        category='Lunch'
+    )
+
+    chicken_avocado_wrap = create_recipe(
+        user=nadia,
+        name='Spicy Chicken & Avocado Wraps',
+        url='https://www.bbcgoodfood.com/recipes/spicy-chicken-avocado-wraps',
+        category='Lunch'
+    )
+
+    ham_cheese_egg_bagel = create_recipe(
+        user=hafsa,
+        name='Air Fryer Ham, Cheese & Egg Bagel',
+        url='https://www.bbcgoodfood.com/recipes/air-fryer-ham-cheese-egg-bagel',
+        category='Lunch'
+    )
+
+    ultimate_chorizo_ciabatta = create_recipe(
+        user=zach,
+        name='Ultimate Chorizo Ciabatta',
+        url='https://www.bbcgoodfood.com/recipes/ultimate-chorizo-ciabatta',
+        category='Lunch'
+    )
+
     chicken_pasta_bake = create_recipe(
-        user=phil,
+        user=hugh,
         name='Chicken Pasta Bake',
         url='https://www.bbcgoodfood.com/recipes/chicken-pasta-bake',
         category='Dinner'
     )
 
+    creamy_chicken_pasta = create_recipe(
+        user=jack,
+        name='Creamy Chicken Pasta',
+        url='https://www.bbcgoodfood.com/recipes/creamy-chicken-pasta',
+        category='Dinner'
+    )
+
+    easy_chicken_curry = create_recipe(
+        user=zach,
+        name='Easy Chicken Curry',
+        url='https://www.bbcgoodfood.com/recipes/easy-chicken-curry',
+        category='Dinner'
+    )
+
+    crispy_chilli_beef = create_recipe(
+        user=jack,
+        name='Air Fryer Crispy Chilli Beef',
+        url='https://www.bbcgoodfood.com/recipes/air-fryer-crispy-chilli-beef',
+        category='Dinner'
+    )
+
     #likes
     create_like(jack, pancakes)
-    create_like(phil, chicken_pasta_bake)
+    create_like(hugh, pancakes)
+    create_like(hafsa, pancakes)
+    create_like(zach, pancakes)
+    create_like(nadia, pancakes)
+    create_like(hafsa, french_toast)
+    create_like(hugh, french_toast)
+    create_like(hugh, chicken_pasta_bake)
     create_like(jack, chicken_pasta_bake)
+    create_like(jack, easy_chicken_curry)
+    create_like(hugh, easy_chicken_curry)
+    create_like(nadia, easy_chicken_curry)
+
 
 if __name__ == '__main__':
     print("Populating database...")
