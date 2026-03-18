5bff9df38aac1207824ac4fe15c90eb1ffa04756
“Hafsa <3117082i@student.gla.ac.uk>
2026-03-18 11:23:02 +0000

    sign-out fix

--- reciperate_app/views.py
+++ reciperate_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.template import loader
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
