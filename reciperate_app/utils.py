2e1a8a3a91088e140eb5d6b3ca9fc058ea7bf1cf
Nadia Hussein <3120663H@student.gla.ac.uk>
2026-03-18 11:03:18 +0000

    added sorting

--- reciperate_app/utils.py
+++ reciperate_app/utils.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 from .models import Recipe, Like
 
 def render_recipe(request, category, template_name):
-    recipes = Recipe.objects.filter(category=category).order_by('-created_at')
-    context_dict = {'recipes': recipes}
+    sort = request.GET.get("sort", "popular")
+
+    if sort == "newest":
+        recipes = Recipe.objects.filter(category=category).order_by("-created_at")
+    elif sort == "oldest":
+        recipes = Recipe.objects.filter(category=category).order_by("created_at")
+    elif sort == "az":
+        recipes = Recipe.objects.filter(category=category).order_by("name")
+    else:
+        recipes = sorted(Recipe.objects.filter(category=category), key=lambda r: r.like_count, reverse=True)
+
+    context_dict = {"recipes": recipes, "sort": sort}
 
     if request.user.is_authenticated:
         context_dict["liked_posts"] = Like.objects.filter(
