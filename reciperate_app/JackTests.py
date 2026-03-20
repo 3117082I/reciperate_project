9008f0d34d4833e3a09623a6e020ca34deb00c62
“Hafsa <3117082i@student.gla.ac.uk>
2026-03-20 21:31:02 +0000

    removed repetition in css and deleted old test files

--- reciperate_app/JackTests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from django.contrib.auth.models import User
-from reciperate_app.models import Recipe, UserProfile, Like
-from population_script import create_user, create_recipe, create_like
-from django.urls import reverse
-
-# Create your tests here.
